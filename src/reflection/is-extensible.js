e0780b19927fe88a96902a2bcf480cba2b7423e8
Mikołaj Lewandowski <mikulew@gmail.com>
2021-04-03 16:21:28 +0200

    Extend example in is-extensible

--- src/reflection/is-extensible.js
+++ src/reflection/is-extensible.js
@@ -8,10 +8,44 @@ let proxy = new Proxy(target, {
   }
 });
 
-console.log(Object.isExtensible(target));
-console.log(Object.isExtensible(proxy));
+console.log("Object.isExtensible(target)", Object.isExtensible(target));
+console.log("Object.isExtensible(proxy)", Object.isExtensible(proxy));
 
 Object.preventExtensions(proxy);
 
-console.log(Object.isExtensible(target));
-console.log(Object.isExtensible(proxy));
+console.log("Object.isExtensible(target)", Object.isExtensible(target));
+console.log("Object.isExtensible(proxy)", Object.isExtensible(proxy));
+
+let targetTurnOffPreventExtensions = {};
+let proxyTurnOffPreventExtensions = new Proxy(targetTurnOffPreventExtensions, {
+  isExtensible(trapTarget) {
+    return Reflect.isExtensible(trapTarget);
+  },
+  preventExtensions(trapTarget) {
+    return false;
+  }
+});
+
+console.log(
+  "Object.isExtensible(targetTurnOffPreventExtensions)",
+  Object.isExtensible(targetTurnOffPreventExtensions)
+);
+console.log(
+  "Object.isExtensible(proxyTurnOffPreventExtensions)",
+  Object.isExtensible(proxyTurnOffPreventExtensions)
+);
+
+Object.preventExtensions(proxyTurnOffPreventExtensions);
+/*
+  TypeError: 'preventExtensions' on proxy: trap returned falsish
+*/
+
+console.log(
+  "Object.isExtensible(targetTurnOffPreventExtensions)",
+  Object.isExtensible(targetTurnOffPreventExtensions)
+);
+console.log(
+  "Object.isExtensible(proxyTurnOffPreventExtensions)",
+  Object.isExtensible(proxyTurnOffPreventExtensions)
+);
+// return false
