5a8980f12372524dd02bf1c4aa7f2374cdb544e8
Mikołaj Lewandowski <mikulew@gmail.com>
2021-04-10 17:00:51 +0200

    Extend NumbersProxy function to previous file

--- src/reflection/only-number-arguments.js
+++ src/reflection/only-number-arguments.js
@@ -19,6 +19,30 @@ let sumProxy = new Proxy(sum, {
 
 console.log(sumProxy(1, 2, 3, 4));
 
-console.log(sumProxy(1, "2", 3, 4)); // TypeError: All arguments have to be number typed.
+// console.log(sumProxy(1, "2", 3, 4)); // TypeError: All arguments have to be number typed.
 
-let result = new sumProxy(); // TypeError: This function cannot be invoked by new operator.
+// let result = new sumProxy(); // TypeError: This function cannot be invoked by new operator.
+
+function Numbers(...values) {
+  this.values = values;
+}
+
+let NumbersProxy = new Proxy(Numbers, {
+  apply() {
+    throw new TypeError("This function have to be invoked by new operator.");
+  },
+  construct(trapTarget, argumentList) {
+    argumentList.forEach((arg) => {
+      if (typeof arg !== "number") {
+        throw new TypeError("All arguments have to be number typed.");
+      }
+    });
+
+    return Reflect.construct(trapTarget, argumentList);
+  }
+});
+
+let instance = new NumbersProxy(1, 2, 3, 4);
+console.log(instance.values);
+
+// NumbersProxy(1, 2, 3, 4); // TypeError: This function have to be invoked by new operator.
