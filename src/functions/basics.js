8fe44c1e6779559c3409734c52843001bf4649e3
Mikołaj Lewandowski <mikulew@gmail.com>
2021-04-25 18:54:20 +0200

    Callback examples

--- src/functions/basics.js
+++ src/functions/basics.js
@@ -6,7 +6,7 @@ console.log("modified", modified);
 // Curring
 const curring = (x) => (y) => x + y;
 const curried = curring(5)(4);
-console.log("curried", curried);
+// console.log("curried", curried);
 
 // Impure function & side effects
 const nums = [1, 2, 3];
@@ -15,4 +15,29 @@ const impure = (arr) => {
   return arr;
 };
 impure(nums);
-console.log("nums", nums);
+// console.log("nums", nums);
+
+// Callbacks
+const multiply = (x, y) => x * y;
+const multiplied = multiply(5, 4);
+console.log("multiplied", multiplied);
+
+const double = (x, callback) => callback(x, 2); // HOF
+
+// Callback #1
+const doubled = double(5, multiply);
+console.log("doubled", doubled);
+
+// Callback #2
+const arrOfNums = [1, 2, 3];
+const filteredArrOfNums = arrOfNums.filter((i) => i % 2);
+console.log("arrOfNums", arrOfNums);
+console.log("filteredArrOfNums", filteredArrOfNums);
+
+// Callbacks #3
+window.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("btn");
+  btn.addEventListener("click", () => {
+    console.log("Clicked!");
+  });
+});
